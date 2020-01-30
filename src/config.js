bf01295bbdcc8dbb9ba414b2929ee9107d477e85
Adam Szczombrowski <adam.szczombrowski@gmail.com>
2020-01-30 13:04:04 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "nots-app-uploads-as"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://v6n3l178gg.execute-api.eu-central-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_bVHA1ec9y",
+    APP_CLIENT_ID: "5fnkj6aqsj30h5213141c3f6en",
+    IDENTITY_POOL_ID: "eu-central-1:65261033-52d3-42df-b746-bab63a98b700"
+  }
+};
